9f39aace9d95bda4c50191bcea4f2b502710a353
aniebietafia <aniebietafia87@gmail.com>
2025-08-07 11:30:42 +0100

    setting up the project.

---
+++ src/main/java/com/portal/mortgage/MortgagePortalApplication.java
@@ -0,0 +1,13 @@
+package com.portal.mortgage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgagePortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MortgagePortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/portal/mortgage/MortgagePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portal.mortgage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortgagePortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
